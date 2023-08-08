6e5fe85de782855d2ab171bdc4b49eba9f95eecd
sanduni <sandunissts@gmail.com>
2023-08-08 11:46:27 +0530

    design the UI

--- lib/View/HomePage.dart
+++ lib/View/HomePage.dart
@@ -8,16 +8,79 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        backgroundColor: Colors.black,
+        backgroundColor: Colors.green[400],
         title: const Text(
           'Tip Calculator',
           style: TextStyle(
-            color: Colors.white,
+            color: Colors.black,
+            fontStyle: FontStyle.italic,
+            fontSize: 30,
           ),
         ),
       ),
       body: Container(
-        color: Colors.black,
+        color: Colors.white,
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextField(
+              decoration: InputDecoration(labelText: 'Bill Amount (\$)'),
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
+            ),
+            SizedBox(height: 16.0),
+            TextField(
+              decoration: InputDecoration(labelText: 'Tip Percentage (%)'),
+              keyboardType: TextInputType.number,
+            ),
+            SizedBox(height: 16.0),
+            TextField(
+              decoration: InputDecoration(labelText: 'Number of People'),
+              keyboardType: TextInputType.number,
+            ),
+            SizedBox(height: 16.0),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text(
+                  'Calculate Tip',
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 32.0),
+            Center(
+              child: Container(
+                alignment: Alignment.centerLeft,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Tip Amount:',
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                    Text(
+                      'Total Amount:',
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                    Text(
+                      'Total Per Person:',
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
